736bdc82189ac687b26236204cde247dd70b0e11
Ignacio <61759306+wisemonkey17@users.noreply.github.com>
2022-08-14 19:51:53 -0300

    Agregación de banner, iconos y acerca de.

--- ProyectoIntegrador_OviedoIgnacio/src/app/app.module.ts
+++ ProyectoIntegrador_OviedoIgnacio/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
