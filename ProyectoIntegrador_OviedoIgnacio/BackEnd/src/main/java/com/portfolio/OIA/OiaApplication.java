658afb59204c9ba6a935b3cafb5c2e02a1080d78
Ignacio <61759306+wisemonkey17@users.noreply.github.com>
2022-08-16 04:40:41 -0300

    Creacion de carpeta "BackEnd"

---
+++ ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/OiaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.OIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OiaApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_OviedoIgnacio/BackEnd/src/test/java/com/portfolio/OIA/OiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.OIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
