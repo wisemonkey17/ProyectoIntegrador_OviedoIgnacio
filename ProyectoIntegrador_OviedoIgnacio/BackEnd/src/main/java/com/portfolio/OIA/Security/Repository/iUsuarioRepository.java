80b0cad848f723ae10202af71fba85616308b5bf
Ignacio <61759306+wisemonkey17@users.noreply.github.com>
2022-08-19 17:52:43 -0300

    Agrego S.
    La falta de S al final de existByEmail y existByNombreUsuario no dejaba compilar el tomcat.

--- ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/Security/Repository/iUsuarioRepository.java
+++ ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/Security/Repository/iUsuarioRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface iUsuarioRepository extends JpaRepository<Usuario, Integer>{
     Optional<Usuario> findByNombreUsuario(String nombreUsuario);
     
-    boolean existByNombreUsuario(String nombreUsuario);
-    boolean existByEmail(String email);
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
 }
