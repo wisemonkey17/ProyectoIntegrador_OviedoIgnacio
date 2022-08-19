6379afe988bb0edee3292c77c7e885976fbf709e
Ignacio <61759306+wisemonkey17@users.noreply.github.com>
2022-08-19 17:50:41 -0300

    Agrego "s".
    La falta de las S al final de existByEmail y existByNombreUsuario no dejaban compilar el tomcat.

--- ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/Security/Controller/AuthController.java
+++ ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/Security/Controller/AuthController.java
@@ -48,10 +48,10 @@ public class AuthController {
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Mensaje("Campos mal puestos o email invalido."), HttpStatus.BAD_REQUEST);
         
-        if(usuarioService.existByNombreUsuario(nuevoUsuario.getNombreUsuario()))
+        if(usuarioService.existsByNombreUsuario(nuevoUsuario.getNombreUsuario()))
             return new ResponseEntity(new Mensaje("Este nombre de usuario ya existe."),HttpStatus.BAD_REQUEST);
         
-        if(usuarioService.existByEmail(nuevoUsuario.getEmail()))
+        if(usuarioService.existsByEmail(nuevoUsuario.getEmail()))
             return new ResponseEntity(new Mensaje("Este email ya es utilizado."),HttpStatus.BAD_REQUEST);
         
         Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(), nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
