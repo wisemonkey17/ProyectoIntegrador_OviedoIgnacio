6709a1e537513c20388d2d6d30cfa8ce4f865ecb
Ignacio <ignaciooviedo18@gmail.com>
2022-08-20 00:17:15 -0300

    Agrego permisos para crear, editar y borrar personas.

--- ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/Controller/PersonaController.java
+++ ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.OIA.Entity.Persona;
 import com.portfolio.OIA.Inteface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,18 +25,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada exitosamente.";
     }
     
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente.";
     }
     
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id, 
                                @RequestParam("nombre") String nuevoNombre, 
