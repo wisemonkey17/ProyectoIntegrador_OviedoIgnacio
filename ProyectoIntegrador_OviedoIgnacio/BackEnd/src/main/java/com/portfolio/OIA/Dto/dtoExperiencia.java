9664d26dad0e6d90a59d8f02d6a8d796ce77a236
Ignacio <ignaciooviedo18@gmail.com>
2022-08-20 21:48:03 -0300

    Integracion de Experiencia en el BackEnd.

---
+++ ProyectoIntegrador_OviedoIgnacio/BackEnd/src/main/java/com/portfolio/OIA/Dto/dtoExperiencia.java
@@ -0,0 +1,41 @@
+package com.portfolio.OIA.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class dtoExperiencia {
+
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+
+    //Constructores
+
+    public dtoExperiencia() {
+    }
+
+    public dtoExperiencia(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+    
+    //Getter & Setter
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+    
+    
+}
