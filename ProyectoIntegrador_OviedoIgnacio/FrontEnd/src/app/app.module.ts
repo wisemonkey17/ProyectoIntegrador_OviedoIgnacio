706454dff689cbb156b4da9e68532d81900511e6
Ignacio <61759306+wisemonkey17@users.noreply.github.com>
2022-08-21 23:07:09 -0300

    Version final de Experiencia.
    No funciona el boton de Borrar.

--- ProyectoIntegrador_OviedoIgnacio/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_OviedoIgnacio/FrontEnd/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
     FooterComponent,
     HomeComponent,
     LoginComponent,
-    NewExperienciaComponent
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
